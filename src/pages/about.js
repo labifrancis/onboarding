b8e546b96242f1010783a649df83d6da364ad766
Labi Francis <moodyblerd@gmail.com>
2022-07-19 11:31:23 +0100

    Updated content

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,7 @@ import Layout from '../components/layout'
 const AboutPage = () => {
   return (
     <Layout pageTitle="About The Painting">
-      <p> What will you like to know? Knowing more about the painting is classified information</p>
+      <p> What will you like to know? Knowing more about the painting is classified information. Yes, it is! </p>
     </Layout>
   )
 }
