aef76f34a87dce51b1b75cf7d60a55c86144b8df
Labi Francis <moodyblerd@gmail.com>
2022-08-19 19:49:43 +0100

    Testing static image

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from "gatsby-plugin-image"
 
 const AboutPage = () => {
   return (
     <Layout pageTitle="About The Painting">
-      <p> What will you like to know? Knowing more about the painting is classified information. Yes, it is! This is the new! This is the other new  </p>
+      <p> What will you like to know? Knowing more about the painting is classified information. Yes, it is! This is the new! This is the other new  
+     <StaticImage src="https://placekitten.com/800/600" alt="A kitten" /></p>
     </Layout>
   )
 }
