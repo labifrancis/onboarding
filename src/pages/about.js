e1bc8ea164b97f43f013c250bf07f9c5305846cd
Labi Francis <moodyblerd@gmail.com>
2022-10-11 10:01:01 +0100

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,7 +5,7 @@ import { StaticImage } from "gatsby-plugin-image"
 const AboutPage = () => {
   return (
     <Layout pageTitle="About The Painting">
-      <p> What will you like to know? Knowing more about the painting is classified information. Yes, it is! This is the new! This is the other new  
+      <p> What will you like to know???? Knowing more about the painting is classified information. Yes, it is! This is the new! This is the other new  
      <StaticImage src="https://placekitten.com/800/600" alt="A kitten" /></p>
     </Layout>
   )
